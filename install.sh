6a2d5b20e01e57654fd0fc2f259ae4c49e94d521
lunik <guillaume.lunik@gmail.com>
2018-01-06 12:48:09 +0100

    add coment

--- install.sh
+++ install.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
+echo "===== Download Tcloud App ====="
 URL=$(curl -s https://api.github.com/repos/lunik/tcloud/releases/latest | grep browser_download_url | cut -d '"' -f 4)
-
 wget $URL -O tcloud.tgz
 
+echo "===== Unarchive App ====="
 mkdir tcloud
-
 tar zxf tcloud.tgz -C tcloud --strip-components 1
 
+echo "===== Install dependencies ====="
 cd tcloud
-
 npm install --only=prod
